3c50c48e943e19f1f841adf6ccdd27c92a561ed4
redbayoub <redayoub47@gmail.com>
2022-12-15 10:18:33 +0100

    customize user attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
+        'isAdmin',
     ];
 
     /**
@@ -39,6 +39,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'isAdmin' => 'boolean',
     ];
 }
