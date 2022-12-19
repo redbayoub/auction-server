6d52b6983506f2d4db070ebbec2639ff5ca39172
redbayoub <redayoub47@gmail.com>
2022-12-19 11:33:56 +0100

    add bot model and migration

---
+++ app/Models/Bot.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bot extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'maxAmount',
+        'percentageAlert',
+        'user_id',
+    ];
+    
+    
+}
---
+++ database/migrations/2022_12_19_090631_create_bots_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bots', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('maxAmount');
+            $table->unsignedTinyInteger('percentageAlert')->default(90);
+            $table->boolean('isAlertSent')->default(false);
+            $table->foreignId('user_id')->constrained();
+            $table->unique('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bots');
+    }
+};
