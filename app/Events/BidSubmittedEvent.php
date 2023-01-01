7daafd288df44923fb8cb9f33fd835a4f90ad072
redbayoub <redayoub47@gmail.com>
2023-01-01 16:20:44 +0100

    add real time update to bid submission

--- app/Events/BidSubmittedEvent.php
+++ app/Events/BidSubmittedEvent.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class BidSubmittedEvent
+class BidSubmittedEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -40,6 +40,6 @@ class BidSubmittedEvent
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new Channel("items." . $this->bid->item_id . ".bids");
     }
 }
